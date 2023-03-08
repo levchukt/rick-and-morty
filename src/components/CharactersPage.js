84d2a5eec731db2fd6f000fc43bcf9f71b6fc841
Taras Levchuk <tarasleva2@gmail.com>
2023-03-08 19:47:44 +0200

    added form to search for Character

--- src/components/CharactersPage.js
+++ src/components/CharactersPage.js
@@ -1,14 +1,54 @@
 import logo from '../img/logo.png'
+import * as Yup from 'yup';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
 
 const CharactersPage = (props) => {
     return (
         <div>
-            <img src={logo} alt='Logo'/>
+            <img src={logo} alt='Logo' />
+            <div>
+                <LoginForm/>
+            </div>
             {props.characters.map(character => {
                     return <div>{character.name}</div>
                 })}
         </div>
     )
+}
+
+
+
+const LoginForm = ({ onSubmit }) => {
+    const handleSubmit = (values) => {
+        console.log(values);
+        // your submission logic here
+    };
+    const initialValues = {
+        field: ''
+    };
+
+    const validationSchema = Yup.object({
+        field: Yup.string().required('Required')
+    })
+
+    return <Formik initialValues={initialValues} validateOnBlur onSubmit={handleSubmit} validationSchema={validationSchema}>
+
+            <Form>
+                <Field
+            type="text"
+            name="field"
+            onChange={(e) => {
+              e.persist();
+              handleSubmit(e.target.value);
+            }}
+            onBlur={(e) => {
+              e.persist();
+              handleSubmit(e.target.value);
+            }}
+          />
+            </Form>
+    </Formik>
 }
 
 export default CharactersPage
